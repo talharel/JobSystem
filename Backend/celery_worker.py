b603b0a58d0245df9540c26e010b3340a8b308cf
Tal Harel <talharel80@gmail.com>
2024-09-08 17:45:16 +0300

    docker compose

--- Backend/celery_worker.py
+++ Backend/celery_worker.py
@@ -2,11 +2,10 @@ from celery import Celery
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 backend = f"db+postgresql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_URL')}"
 
 celery_app = Celery('celery_tasks',
-                    broker='amqp://guest:guest@localhost:5672/',
+                    broker=os.getenv('CELERY_BROKER'),
                     backend=backend,
-                    include=['celery_tasks.jobs','celery_tasks.companies'])
+                    include=['celery_tasks.jobs', 'celery_tasks.companies'])
